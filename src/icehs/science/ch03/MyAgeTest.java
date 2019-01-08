8ccd952bf3d03688ebaf117e98e507ae16b25b23
admin <admin@10.137.138.103>
2019-01-08 17:31:11 +0900

    AgeTestHaha

---
+++ src/icehs/science/ch03/MyAgeTest.java
@@ -0,0 +1,14 @@
+package icehs.science.ch03;
+
+public class MyAgeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myBirthYear = 1990;
+		int myAge = 24;
+		System.out.println(myBirthYear);
+		System.out.println(myAge);
+
+	}
+
+}
