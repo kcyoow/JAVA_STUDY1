e10d90bfbeda3dc54fbe0434c463400e3ad1dcbe
admin <admin@10.137.138.103>
2019-01-08 17:46:58 +0900

    fix some errors

---
+++ src/icehs/science/ch03/VariableHelpEx.java
@@ -0,0 +1,22 @@
+package icehs.science.ch03;
+
+public class VariableHelpEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a1234;
+		//delete # 
+		int first;
+		//add 1
+		int class1;
+		int var;
+		//add 1
+		int var1;
+		//input 20
+		int number = 20;
+		
+		System.out.println(number);
+
+	}
+
+}
