74c8f909463babfed3ca95a5e907062de3932293
admin <admin@10.137.138.103>
2019-01-08 18:02:17 +0900

    Just Test

---
+++ src/icehs/science/ch03/StatisticsTest.java
@@ -0,0 +1,17 @@
+package icehs.science.ch03;
+
+public class StatisticsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int fromSunToEarthDistance = 150000000;
+		double lottoProbability = 0.0000001235;
+		long population = 6973738433L;
+		boolean isRight = true;
+		System.out.println(fromSunToEarthDistance);
+		System.out.println(lottoProbability);
+		System.out.println(population);
+		System.out.println(isRight);
+	}
+
+}
