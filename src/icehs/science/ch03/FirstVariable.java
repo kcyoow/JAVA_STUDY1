ffd0951cc2a0b55028ad9f2af364a24c8e12b070
admin <admin@10.137.138.103>
2019-01-08 17:16:58 +0900

    learning

---
+++ src/icehs/science/ch03/FirstVariable.java
@@ -0,0 +1,14 @@
+package icehs.science.ch03;
+
+public class FirstVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Data_Type 변수명;
+		//같이 해보는 실습시간
+		int money = 5000;
+		System.out.println(money);
+
+	}
+
+}
