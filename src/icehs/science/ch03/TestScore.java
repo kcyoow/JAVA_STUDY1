e863a5f133df5fe4651bd6c9d28e27f787e02e23
admin <admin@10.137.138.103>
2019-01-08 17:37:08 +0900

    TestScoreTest

---
+++ src/icehs/science/ch03/TestScore.java
@@ -0,0 +1,17 @@
+package icehs.science.ch03;
+
+public class TestScore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Java_Programing, SQL, Servlet_And_JSP;
+		Java_Programing = 100;
+		SQL = 80;
+		Servlet_And_JSP = 85;
+		System.out.println(Java_Programing);
+		System.out.println(SQL);
+		System.out.println(Servlet_And_JSP);
+
+	}
+
+}
