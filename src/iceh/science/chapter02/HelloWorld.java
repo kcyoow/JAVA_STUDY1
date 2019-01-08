7aed69e278c88e85c01c28572841fb959e03cfbe
admin <admin@10.137.138.103>
2019-01-08 16:22:48 +0900

    Hello

---
+++ src/iceh/science/chapter02/HelloWorld.java
@@ -0,0 +1,10 @@
+package iceh.science.chapter02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("¾È³ç ¼¼°è!");
+	}
+}
