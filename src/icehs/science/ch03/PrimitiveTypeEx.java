197b7de191d4ac3dac5c6e80421a232446a2df79
admin <admin@10.137.138.103>
2019-01-08 17:57:46 +0900

    It's just primitive

---
+++ src/icehs/science/ch03/PrimitiveTypeEx.java
@@ -0,0 +1,18 @@
+package icehs.science.ch03;
+
+public class PrimitiveTypeEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myAge = 27;
+		long billWealth = 7000000000L;
+		double pi = 3.1415926535;
+		boolean isRight = true;
+		System.out.println("my age : " + myAge);
+		System.out.println("bill's wealth : " + billWealth);
+		System.out.println("pi : " + pi);
+		System.out.println("That all correct? : " + isRight);
+
+	}
+
+}
