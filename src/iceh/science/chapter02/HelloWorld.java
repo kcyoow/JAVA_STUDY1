c83d74406c759a0a3a4e864cc95dff92e5df8d5a
admin <admin@10.137.138.103>
2019-01-08 16:33:36 +0900

    new update v2

--- src/iceh/science/chapter02/HelloWorld.java
+++ src/iceh/science/chapter02/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("안녕 세계!");
 		System.out.println("안녕 세계! 복사본");
+		System.out.println("What R U Doing???");
 	}
 }
