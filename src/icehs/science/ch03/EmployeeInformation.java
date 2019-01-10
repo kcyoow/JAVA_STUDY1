d499952b64c49e42d8ea1475b62ad38b63367440
admin <admin@10.137.138.103>
2019-01-10 16:08:28 +0900

    new

--- src/icehs/science/ch03/EmployeeInformation.java
+++ src/icehs/science/ch03/EmployeeInformation.java
@@ -5,12 +5,15 @@ public class EmployeeInformation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name = "홍길동";
-		int year = 2002;
-		int workingYears = 15;
+		String enterYear = "1995";
+		int workingYears = 2019;
+		int intEnterYear = Integer.parseInt(enterYear);
+		int employedYear = workingYears - intEnterYear;
 		System.out.println("이름 : " + name);
-		System.out.println("입사년도 : " + year);
-		System.out.println("근무년수 : " + workingYears);
-
+		System.out.println("입사년도 : " + enterYear);
+		System.out.println("근무년수 : " + employedYear);
+		String sys = "[system]";
+		System.out.println(sys + "안녕 여러분!");
 	}
 
 }
