528135306b091549bcd8ad02b4c4e93645008977
admin <admin@10.137.138.103>
2019-01-10 17:03:10 +0900

    new 3

--- src/icehs/science/ch04/TemperatureTest.java
+++ src/icehs/science/ch04/TemperatureTest.java
@@ -6,7 +6,7 @@ public class TemperatureTest {
 		int fahrenheit = 100;
 		double celcius = 0;
 		
-		celcius = (double)5 / 9 * (fahrenheit - 32);
+		celcius = (double) 5 / 9 * (fahrenheit - 32);
 		System.out.println("È­¾¾ : " + fahrenheit);
 		System.out.println("¼·¾¾ : " + celcius);
 
