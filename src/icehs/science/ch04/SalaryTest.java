c9a5f9cd7aac140f5fd4fd1b4818be0c5bf4d763
kcyoow <kcy021012@gmail.com>
2019-01-11 01:05:16 +0900

    new

--- src/icehs/science/ch04/SalaryTest.java
+++ src/icehs/science/ch04/SalaryTest.java
@@ -3,13 +3,14 @@ package icehs.science.ch04;
 public class SalaryTest {
 
 	public static void main(String[] args) {
-		int aYearMoney = 12000000;
-		long taxAYearMoney = aYearMoney - aYearMoney / 10;
-		System.out.println("연봉 : " + aYearMoney + ", " + "세후 연봉 : " + taxAYearMoney);
-		int bonus = 800000;
-		double taxBonus = bonus - (bonus *0.055);
-		System.out.println("보너스 : " + bonus + ", " + "세후 보너스 : " + (int)taxBonus);
-		System.out.println("지급액 : " + (int)(taxAYearMoney + taxBonus));
+		int Salary = 1000000;
+		int aYearSalary = Salary * 12;
+		int taxAYearSalary = aYearSalary - (int)(aYearSalary * 0.1);
+		System.out.println("연봉 : " + aYearSalary + ", " + "세후 연봉 : " + taxAYearSalary);
+		int bonus = Salary - (int)(Salary * 0.2);
+		int taxBonus = bonus - (int)(bonus *0.055);
+		System.out.println("보너스 : " + bonus + ", " + "세후 보너스 : " + taxBonus);
+		System.out.println("지급액 : " + (taxAYearSalary + taxBonus));
 	}
 
 }
