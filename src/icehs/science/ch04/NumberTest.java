7a894494b4efba5c5b18d5d271248b9ae8c2b9a3
admin <admin@10.137.138.103>
2019-01-10 17:45:59 +0900

    new

---
+++ src/icehs/science/ch04/NumberTest.java
@@ -0,0 +1,15 @@
+package icehs.science.ch04;
+
+public class NumberTest {
+
+	public static void main(String[] args) {
+		int num = 10;
+		
+		System.out.println("10은 양수입니까? : " + (num >= 0));
+		System.out.println("10은 짝수입니까? : " + (num%2 == 0));
+		System.out.println("10은 홀수입니까? : " + (num%2 != 0));
+		System.out.println("10은 30보다 큽니까? : " + (num > 30));
+
+	}
+
+}
---
+++ src/icehs/science/ch04/SeperateNumberTest.java
@@ -0,0 +1,19 @@
+package icehs.science.ch04;
+
+public class SeperateNumberTest {
+
+	public static void main(String[] args) {
+		int number = 456;
+		int number100 = 456 / 100;
+		number100 = number % 100;
+		int number10 = number100 / 10;
+		number10 = number100 % 10;
+		int number1 = number10 / 1;
+		
+		System.out.println("숫자 : " + number);
+		System.out.println("백의 자리 수 : " + number100);	
+		System.out.println("십의 자리 수 : " + number10);
+		System.out.println("일의 자리 수 : " + number1);
+	}
+
+}
