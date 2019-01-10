3e41aab33e597211763728520d7c7cddfbe68bcd
admin <admin@10.137.138.103>
2019-01-10 14:59:25 +0900

    new

--- src/icehs/science/ch03/CastEx.java
+++ src/icehs/science/ch03/CastEx.java
@@ -4,5 +4,8 @@ public class CastEx {
 	public static void main(String[] args) {
 		double large = 1000;
 		long middle = (long)large;
+		
+		System.out.println(large);
+		System.out.println(middle);
 	}
 }
