80ccb183248e3cf8527d3559544dc4a1ddb52420
admin <admin@10.137.138.103>
2019-01-10 15:40:11 +0900

    new

--- src/icehs/science/ch03/StudentEx.java
+++ src/icehs/science/ch03/StudentEx.java
@@ -2,4 +2,14 @@ package icehs.science.ch03;
 
 public class StudentEx {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "길순이";
+		int grade = 2;
+		String address = "인천시 남구 도화동";
+		System.out.println(name + " : " + address);
+		System.out.println(grade + "학년");
+		System.out.println("이름 : " + name);
+	}
+
 }
